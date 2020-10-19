29586c84d6c5c3ab221b4eaf0c0c80859005abc0
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-10-19 10:47:24 +0900

    Iterator Pattern with Simple Name Repo

---
+++ src/com/mezonworks/iterator_pattern/with_simple_name_repo/Container.java
@@ -0,0 +1,5 @@
+package com.mezonworks.iterator_pattern.with_simple_name_repo;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ src/com/mezonworks/iterator_pattern/with_simple_name_repo/Iterator.java
@@ -0,0 +1,6 @@
+package com.mezonworks.iterator_pattern.with_simple_name_repo;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
