2c598b6657b588e9b210c49d5a2b0198168b84a4
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-10-19 15:29:36 +0900

    Memento Pattern with Simple Example

---
+++ src/com/mezonworks/memento_pattern/with_simple_example/Memento.java
@@ -0,0 +1,13 @@
+package com.mezonworks.memento_pattern.with_simple_example;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
