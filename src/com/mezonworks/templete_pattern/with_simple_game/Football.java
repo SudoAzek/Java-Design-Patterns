510da58b72078ced8e2ed5c0568d6a75ebbd8093
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-10-20 09:28:05 +0900

    Templete Pattern with Simple Game

---
+++ src/com/mezonworks/templete_pattern/with_simple_game/Football.java
@@ -0,0 +1,18 @@
+package com.mezonworks.templete_pattern.with_simple_game;
+
+public class Football extends Game {
+    @Override
+    void initialize() {
+        System.out.println("Football Game Initialized!");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Football Game Started!");
+    }
+
+    @Override
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    }
+}
---
+++ src/com/mezonworks/templete_pattern/with_simple_game/TemplatePatternDemo.java
@@ -0,0 +1,11 @@
+package com.mezonworks.templete_pattern.with_simple_game;
+
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
